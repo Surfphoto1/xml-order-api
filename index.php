d16ed415391529c9805e3974a4926b19420cd51b
Surfphoto1 <oculusphoto1@gmail.com>
2025-07-02 10:12:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,17 @@ header("Content-Type: text/plain");
 // Get raw POST XML from client
 $data = file_get_contents("php://input");
 
-// Send to Honey's Place
-$url = "https://www.honeysplace.com/ws/";
+// Debug: Save the raw XML input
+file_put_contents('debug_input.xml', $data);
+
+// Build POST body for Honey's Place (URL-encoded XML)
 $postFields = "xmldata=" . urlencode($data);
 
-// Use cURL for the request
+// Debug: Save the encoded POST fields
+file_put_contents('debug_postfields.txt', $postFields);
+
+// Send to Honey's Place
+$url = "https://www.honeysplace.com/ws/";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -18,6 +24,7 @@ curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Only for dev/test. Set to true in production!
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; HoneyClient/1.0)');
 
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
